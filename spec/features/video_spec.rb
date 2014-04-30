a9ec953aeafccbfaae6ffa40fa05bb3d4cd0222e
marthaberner <marthaberner.mb@gmail.com>
2014-04-30 14:10:55 -0600

    [#70291446] Use can show a video and play it

--- spec/features/video_spec.rb
+++ spec/features/video_spec.rb
@@ -15,4 +15,20 @@ feature 'User can CRUD a video' do
     expect(page).to have_content 'Video successfully created'
     expect(current_path).to eq video_path(Video.where(description: 'Dancing Baby').first)
   end
+
+  scenario '' do
+    visit '/'
+    click_on 'all videos'
+    click_on 'New Video'
+    fill_in 'video[url]', with: 'http://youtu.be/bgXPl3XM_NA'
+    fill_in 'video[description]', with: 'Dancing Baby'
+    fill_in 'video[rating]', with: '5'
+    click_on 'Add Video'
+    click_on 'all videos'
+    click_on 'http://youtu.be/bgXPl3XM_NA'
+    expect(page).to have_content 'Dancing Baby'
+    expect(page).to have_content 'http://youtu.be/bgXPl3XM_NA'
+    expect(page).to have_content '5'
+    expect(current_path).to eq video_path(Video.where(description: 'Dancing Baby').first)
+  end
 end
